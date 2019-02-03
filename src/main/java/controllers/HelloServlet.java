63481faee657841b7e9523c81853fc143ceceebc
unsOmen <unsomen0405@gmail.com>
2019-02-03 14:15:01 +0600

    test

---
+++ src/main/java/controllers/HelloServlet.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+            throws IOException {
+        httpServletResponse.getWriter().print("Hello from servlet");
+    }
+}
